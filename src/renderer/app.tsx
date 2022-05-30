7ea7865c75c468af4b9f6b34b7ddfc5a63297ef7
nasum <tomato.wonder.life@gmail.com>
2022-05-30 23:26:13 +0900

    add translate-area

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,6 +1,46 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("app"));
 
-root.render(<h2>Hello from React!</h2>);
+const TransLateArea = () => {
+  const [leftText, setLeftText] = useState<string>("");
+  const [rightText, setRightText] = useState<string>("");
+
+  const onLeftTextareaChante = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setLeftText(e.target.value);
+    setRightText(e.target.value);
+  };
+
+  const onRightTextareaChante = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setRightText(e.target.value);
+    setLeftText(e.target.value);
+  };
+  return (
+    <div className="translate-area">
+      <div>
+        <h3>日</h3>
+        <textarea
+          className="left-area"
+          value={leftText}
+          onChange={onLeftTextareaChante}
+        ></textarea>
+      </div>
+      <div>
+        <h3>英</h3>
+        <textarea
+          className="right-area"
+          value={rightText}
+          onChange={onRightTextareaChante}
+        ></textarea>
+      </div>
+    </div>
+  );
+};
+
+root.render(
+  <div>
+    <h2>TranslatePad</h2>
+    <TransLateArea />
+  </div>
+);
